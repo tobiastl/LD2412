d10e5e17f0d26b1974bee042753ee53b919a311d
tobiastl <193034304+tobiastl@users.noreply.github.com>
2025-08-12 22:34:24 -0400

    [Fix] -> Various Issues
    Change overview:
    -Fixed major issue with ack verification
    -Fixed issues with parameters for certain overloaded functions
    -Fixed issue with get firmware version command
    -Fixed issue with set baudrate command
    -Fixed issue with check calibration mode command

--- src/LD2412.cpp
+++ src/LD2412.cpp
@@ -1,8 +1,8 @@
 /**
  * @file LD2412.cpp
  * @author Trent Tobias
- * @version 1.0.0
- * @date August 07, 2025
+ * @version 1.0.1
+ * @date August 12, 2025
  * @brief LD2412 serial communication implementation
  */
 
@@ -19,24 +19,40 @@ void LD2412::sendCommand(uint8_t* data) {
     this->serial.write(this->data_len, 2);
     this->serial.write(data, this->data_len[0]);
     this->serial.write(FRAME_FOOTER, 4);
+
+    this->serial.flush();
 }
 
 uint8_t* LD2412::getAck(uint8_t respData, uint8_t len) {
-    unsigned long time = CURRENT_TIME;
+    unsigned long time = CURRENT_TIME_MS;
     int i = 0;
+    delay(20);
 
-    while (CURRENT_TIME - time < ACK_TIMEOUT)
-        while (this->serial.available() && i < BUFFER_SIZE)
-            this->buffer[i++] = this->serial.read();
+    while (this->serial.available() && i < len) {
+        for (i=0; i<len; i++) {
+            this->buffer[i] = this->serial.read();
+
+            //Ensures packet capture is properly aligned at header
+            if (this->buffer[0] != FRAME_HEADER[0]
+                || i>0 && this->buffer[1] != FRAME_HEADER[1]
+                || i>1 && this->buffer[2] != FRAME_HEADER[2]
+                || i>2 && this->buffer[3] != FRAME_HEADER[3])
+                i=-1;
+
+            //Ack timeout
+            if (CURRENT_TIME_MS - time > ACK_TIMEOUT)
+                return nullptr;
+        }
+    }
 
     if (i >= len) {
         for (i=0; i<len; i++)
             if (i<4 && this->buffer[i] != FRAME_HEADER[i]                       //Verifies header
-                || i==4 && this->buffer[i] != len-12                            //Verifies expected length
+                || i==4 && this->buffer[i] != len-10                            //Verifies expected length
                 || i==5 && this->buffer[i] != 0x00                              //Verifies spacing (0x00)
                 || i==6 && this->buffer[i] != respData                          //Verifies command word for ack
                 || i==7 && this->buffer[i] != 0x01                              //Verifies response acknowledgement (0x01)
-                || len-4<i<len && this->buffer[i] != FRAME_FOOTER[i-(len-4)])   //Verifies footer
+                || i>=len-4 && this->buffer[i] != FRAME_FOOTER[i-(len-4)])      //Verifies footer
                 return nullptr;
     }
     return this->buffer;
@@ -55,18 +71,18 @@ bool LD2412::disableConfig() {
     uint8_t data[] = {0xFE, 0x00};
     sendCommand(data);
 
-    if (const uint8_t* ack = getAck(data[0], 18); ack != nullptr && ack[8] == 0x00)
+    if (const uint8_t* ack = getAck(data[0], 14); ack != nullptr && ack[8] == 0x00)
         return true;
     return false;
 }
 
 bool LD2412::readSerial() {
     //If serial was already successfully read within the past threshold, this function is skipped
-    if (this->serialLastRead != NULL && CURRENT_TIME - this->serialLastRead < this->refresh_threshold)
+    if (this->serialLastRead != NULL && CURRENT_TIME_MS - this->serialLastRead < this->refresh_threshold)
         return true;
 
     int i = 0;
-    long int timeRef = CURRENT_TIME;
+    long int timeRef = CURRENT_TIME_MS;
     while (this->serial.available() && i < 21) {
         for (i=0; i<21; i++) {
             this->buffer[i] = this->serial.read();
@@ -85,11 +101,11 @@ bool LD2412::readSerial() {
                 || i>19 && this->buffer[20] != 0xF5)
                 return false;
 
-            else if (CURRENT_TIME - timeRef > ACK_TIMEOUT)
+            else if (CURRENT_TIME_MS - timeRef > ACK_TIMEOUT)
                 return false;
         }
     }
-    this->serialLastRead = CURRENT_TIME;
+    this->serialLastRead = CURRENT_TIME_MS;
     for (i=0; i<21; i++)
         this->serialBuffer[i] = this->buffer[i];
     return true;
@@ -100,7 +116,8 @@ bool LD2412::enterCalibrationMode() {
     bool success = false;
 
     if (!enableConfig())
-        return false;
+        if (!enableConfig())
+            return false;
     sendCommand(data);
 
     if (const uint8_t* ack = getAck(data[0], 14); ack != nullptr && ack[8] == 0x00)
@@ -113,22 +130,25 @@ int LD2412::checkCalibrationMode() {
     uint8_t data[] = {0x1B, 0x00};
 
     if (!enableConfig())
-        return -1;
+        if (!enableConfig())
+            return -1;
     sendCommand(data);
 
     if (const uint8_t* ack = getAck(data[0], 16); ack != nullptr && ack[8] == 0x00) {
+        uint8_t temp = ack[10];
         disableConfig();
-        return ack[10];
+        return temp;
     }
     disableConfig();
     return -1;
 }
 
 int* LD2412::readFirmwareVersion() {
-    uint8_t data[] = {0x12, 0x00};
+    uint8_t data[] = {0xA0, 0x00};
 
     if (!enableConfig())
-        return nullptr;
+        if (!enableConfig())
+            return nullptr;
     sendCommand(data);
 
     if (const uint8_t* ack = getAck(data[0], 22); ack != nullptr && ack[8] == 0x00) {
@@ -147,7 +167,8 @@ bool LD2412::resetDeviceSettings() {
     bool success = false;
 
     if (!enableConfig())
-        return false;
+        if (!enableConfig())
+            return false;
     sendCommand(data);
 
     if (const uint8_t* ack = getAck(data[0], 14); ack != nullptr && ack[8] == 0x00)
@@ -161,7 +182,8 @@ bool LD2412::restartModule() {
     bool success = false;
 
     if (!enableConfig())
-        return false;
+        if (!enableConfig())
+            return false;
     sendCommand(data);
 
     if (const uint8_t* ack = getAck(data[0], 14); ack != nullptr && ack[8] == 0x00)
@@ -176,7 +198,8 @@ bool LD2412::setParamConfig(uint8_t min, uint8_t max, uint8_t duration, uint8_t 
     bool success = false;
 
     if (!enableConfig())
-        return false;
+        if (!enableConfig())
+            return false;
     sendCommand(data);
 
     if (const uint8_t* ack = getAck(data[0], 14); ack != nullptr && ack[8] == 0x00)
@@ -192,7 +215,8 @@ bool LD2412::setMotionSensitivity(uint8_t sen) {
     bool success = false;
 
     if (!enableConfig())
-        return false;
+        if (!enableConfig())
+            return false;
     sendCommand(data);
 
     if (const uint8_t* ack = getAck(data[0], 14); ack != nullptr && ack[8] == 0x00)
@@ -207,7 +231,8 @@ bool LD2412::setMotionSensitivity(uint8_t sen[14]) {
     bool success = false;
 
     if (!enableConfig())
-        return false;
+        if (!enableConfig())
+            return false;
     sendCommand(data);
 
     if (const uint8_t* ack = getAck(data[0], 14); ack != nullptr && ack[8] == 0x00)
@@ -223,7 +248,8 @@ bool LD2412::setStaticSensitivity(uint8_t sen) {
     bool success = false;
 
     if (!enableConfig())
-        return false;
+        if (!enableConfig())
+            return false;
     sendCommand(data);
 
     if (const uint8_t* ack = getAck(data[0], 14); ack != nullptr && ack[8] == 0x00)
@@ -238,7 +264,8 @@ bool LD2412::setStaticSensitivity(uint8_t sen[14]) {
     bool success = false;
 
     if (!enableConfig())
-        return false;
+        if (!enableConfig())
+            return false;
     sendCommand(data);
 
     if (const uint8_t* ack = getAck(data[0], 14); ack != nullptr && ack[8] == 0x00)
@@ -248,31 +275,31 @@ bool LD2412::setStaticSensitivity(uint8_t sen[14]) {
 }
 
 bool LD2412::setBaudRate(int baud) {
-    uint8_t data[] = {0x05, 0x00};
+    uint8_t data[] = {0xA1, 0x00, 0x05, 0x00};
     switch (baud) {
         case 9600:
-            data[0] = 0x01;
+            data[2] = 0x01;
             break;
         case 19200:
-            data[0] = 0x02;
+            data[2] = 0x02;
             break;
         case 38400:
-            data[0] = 0x03;
+            data[2] = 0x03;
             break;
         case 57600:
-            data[0] = 0x04;
+            data[2] = 0x04;
             break;
         case 115200:
-            data[0] = 0x05;
+            data[2] = 0x05;
             break;
         case 230400:
-            data[0] = 0x06;
+            data[2] = 0x06;
             break;
         case 256000:
-            data[0] = 0x07;
+            data[2] = 0x07;
             break;
         case 460800:
-            data[0] = 0x08;
+            data[2] = 0x08;
             break;
         default:
             return false;
@@ -280,7 +307,8 @@ bool LD2412::setBaudRate(int baud) {
     bool success = false;
 
     if (!enableConfig())
-        return false;
+        if (!enableConfig())
+            return false;
     sendCommand(data);
 
     if (const uint8_t* ack = getAck(data[0], 14); ack != nullptr && ack[8] == 0x00)
@@ -298,7 +326,8 @@ int* LD2412::getParamConfig() {
     uint8_t data[] = {0x12, 0x00};
 
     if (!enableConfig())
-        return nullptr;
+        if (!enableConfig())
+            return nullptr;
     sendCommand(data);
 
     if (const uint8_t* ack = getAck(data[0], 19); ack != nullptr && ack[8] == 0x00) {
@@ -311,27 +340,12 @@ int* LD2412::getParamConfig() {
     return nullptr;
 }
 
-int* LD2412::getMotionSensitivity(RETURN_ARRAY_TRUE) {
+int LD2412::getMotionSensitivity() {
     uint8_t data[] = {0x13, 0x00};
 
     if (!enableConfig())
-        return nullptr;
-    sendCommand(data);
-
-    if (const uint8_t* ack = getAck(data[0], 28); ack != nullptr && ack[8] == 0x00) {
-        for (int i=10; i<24; i++)
-            this->sensResponse[i-10] = static_cast<int>(ack[i]);
-        disableConfig();
-        return this->sensResponse;
-    }
-    disableConfig();
-    return nullptr;
-}
-int LD2412::getMotionSensitivity(RETURN_ARRAY_FALSE) {
-    uint8_t data[] = {0x13, 0x00};
-
-    if (!enableConfig())
-        return false;
+        if (!enableConfig())
+            return false;
     sendCommand(data);
 
     if (const uint8_t* ack = getAck(data[0], 28); ack != nullptr && ack[8] == 0x00) {
@@ -345,12 +359,12 @@ int LD2412::getMotionSensitivity(RETURN_ARRAY_FALSE) {
     disableConfig();
     return -1;
 }
-
-int* LD2412::getStaticSensitivity(RETURN_ARRAY_TRUE) {
-    uint8_t data[] = {0x14, 0x00};
+int* LD2412::getMotionSensitivity(std::true_type) {
+    uint8_t data[] = {0x13, 0x00};
 
     if (!enableConfig())
-        return nullptr;
+        if (!enableConfig())
+            return nullptr;
     sendCommand(data);
 
     if (const uint8_t* ack = getAck(data[0], 28); ack != nullptr && ack[8] == 0x00) {
@@ -362,11 +376,13 @@ int* LD2412::getStaticSensitivity(RETURN_ARRAY_TRUE) {
     disableConfig();
     return nullptr;
 }
-int LD2412::getStaticSensitivity(RETURN_ARRAY_FALSE) {
+
+int LD2412::getStaticSensitivity() {
     uint8_t data[] = {0x14, 0x00};
 
     if (!enableConfig())
-        return false;
+        if (!enableConfig())
+            return false;
     sendCommand(data);
 
     if (const uint8_t* ack = getAck(data[0], 28); ack != nullptr && ack[8] == 0x00) {
@@ -379,6 +395,23 @@ int LD2412::getStaticSensitivity(RETURN_ARRAY_FALSE) {
     }
     disableConfig();
     return -1;
+}
+int* LD2412::getStaticSensitivity(std::true_type) {
+    uint8_t data[] = {0x14, 0x00};
+
+    if (!enableConfig())
+        if (!enableConfig())
+            return nullptr;
+    sendCommand(data);
+
+    if (const uint8_t* ack = getAck(data[0], 28); ack != nullptr && ack[8] == 0x00) {
+        for (int i=10; i<24; i++)
+            this->sensResponse[i-10] = static_cast<int>(ack[i]);
+        disableConfig();
+        return this->sensResponse;
+    }
+    disableConfig();
+    return nullptr;
 }
 
 unsigned int LD2412::getSerialRefreshThres() {
